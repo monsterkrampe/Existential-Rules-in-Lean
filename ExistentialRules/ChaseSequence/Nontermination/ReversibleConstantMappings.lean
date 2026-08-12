f746d0fa569a15266d824a0cbc5a70e184e8b1b7
monsterkrampe <monsterkrampe@users.noreply.github.com>
2026-08-12 13:38:08 +0200

    Define reversible constant mappings

---
+++ ExistentialRules/ChaseSequence/Nontermination/ReversibleConstantMappings.lean
@@ -0,0 +1,34 @@
+/-
+Copyright 2026 Lukas Gerlach
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
+module
+
+public import ExistentialRules.ChaseSequence.Termination.ConstantMappings.Basic
+import BasicLeanDatastructures.Function.InjectiveSurjective
+
+open CustomBasicDatastructures
+
+/-!
+# Reversible Constant Mappings
+
+Here, we define what it means for a `ConstantMapping` to be reversible.
+-/
+
+public section
+
+variable {sig : Signature} [DecidableEq sig.P] [DecidableEq sig.C] [DecidableEq sig.V]
+
+namespace ConstantMapping
+
+/-- This is Definition 10 from the [RPC] paper but we do not demand here that `ts` is closed under subterms. -/
+def isReversible (g : ConstantMapping sig) (ts : Set (GroundTerm sig)) : Prop :=
+  Function.injectiveSet g.apply_ground_term ts ∧
+  ∀ c : sig.C, .const c ∈ ts -> ∀ s ∈ (g c).subterms,
+    ∀ func_u ts_u arity_ok_u, GroundTerm.func func_u ts_u arity_ok_u ∈ ts ->
+    g.apply_ground_term (GroundTerm.func func_u ts_u arity_ok_u) ≠ s
+
+end ConstantMapping
+
+
