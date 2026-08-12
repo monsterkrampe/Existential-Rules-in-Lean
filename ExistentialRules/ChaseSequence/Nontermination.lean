ea86518cd066f95359caac5c6cca141e42dd822f
monsterkrampe <monsterkrampe@users.noreply.github.com>
2026-08-12 15:19:07 +0200

    Rename RpcLike.lean to CyclicitySequence.lean

--- ExistentialRules/ChaseSequence/Nontermination.lean
+++ ExistentialRules/ChaseSequence/Nontermination.lean
@@ -7,11 +7,22 @@ module
 
 import ExistentialRules.ChaseSequence.Nontermination.BirthFacts
 import ExistentialRules.ChaseSequence.Nontermination.CondenseGenerator
+import ExistentialRules.ChaseSequence.Nontermination.CyclicitySequence
 import ExistentialRules.ChaseSequence.Nontermination.HeadChoice
 import ExistentialRules.ChaseSequence.Nontermination.RepeatableUnblockability
 import ExistentialRules.ChaseSequence.Nontermination.ReversibleConstantMappings
-import ExistentialRules.ChaseSequence.Nontermination.RpcLike
 import ExistentialRules.ChaseSequence.Nontermination.SparseSubderivationGenerator
 import ExistentialRules.ChaseSequence.Nontermination.TermSkeleton
 import ExistentialRules.ChaseSequence.Nontermination.Unblockability
+
+/-!
+# RPC-like Non-Termination
+
+We are going to formalize sufficient conditions for chase non-termination.
+Mainly, we will introduce the necessary machinery from Restricted Prefix Cyclicity (RPC) [RPC]
+but we also aim to generalize this to capture (Disjunctive) Model-Faithful Cyclicity ((D)MFC) [DMFA] [RMFA] at the same time.
+
+SO FAR, WE ONLY HAVE A FEW VERY BASIC DEFINITIONS. THERE IS A LONG WAY TO GO.
+-/
+
 
--- ExistentialRules/ChaseSequence/Nontermination/CyclicitySequence.lean
+++ ExistentialRules/ChaseSequence/Nontermination/CyclicitySequence.lean
@@ -11,13 +11,10 @@ import ExistentialRules.ChaseSequence.Nontermination.SparseSubderivationGenerato
 public import ExistentialRules.ChaseSequence.Nontermination.Unblockability
 
 /-!
-# RPC-like Non-Termination
+# Cyclicity Derivations and Branches
 
-We are going to formalize sufficient conditions for chase non-termination.
-Mainly, we will introduce the necessary machinery from Restricted Prefix Cyclicity (RPC) [RPC]
-but we also aim to generalize this to capture (Disjunctive) Model-Faithful Cyclicity ((D)MFC) [DMFA] [RMFA] at the same time.
-
-SO FAR, WE ONLY HAVE A FEW VERY BASIC DEFINITIONS. THERE IS A LONG WAY TO GO.
+This file formalizes what is called "cyclicity sequences" in the [RPC] paper (Section 3) including the main result
+that the existence of a `CyclicityBranch` implies that the given rule set `RuleSet.neverTerminates` (see `CyclicityBranch.neverTerminates_of_cyclicityBranch`).
 -/
 
 public section
